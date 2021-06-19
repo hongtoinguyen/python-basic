2ec43731ea32b1fb03d72de4dec0969cef84635d
Nguyễn Hồng Tới <nguyenhongtoi1@gmail.com>
2021-06-19 15:21:06 +0700

    Done Chapter 4

---
+++ Chapter 4. CÁC VÒNG LẶP/Chap4Exercise-KiemTraSoNguyenTo.py
@@ -0,0 +1,28 @@
+"""
+EXERCISE: Viết chương trình nhập vào một số, kiểm tra xem số này có phải là số nguyên tố hay không.
+          Hỏi người dùng có tiếp tục sử dụng hay thoát phần mềm
+**SOLVE:
+        snt là số chia hết cho 1 và chính nó
+"""
+# print("Chương trình kiểm tra một số có phải số nguyên tố hay không")
+# while True:
+#     n=int(input("Nhập số nguyên dương: "))
+#     count=0
+#     for i in range(1,n+1):
+#         if n % i == 0:
+#             count=count+1
+#     if count==2:
+#         print(n,"là số nguyên tố")
+#     else:
+#         print(n,"không phải là số nguyên tố")
+#     ask=input("Tiếp tục sử dụng phần mềm? (y/n): ")
+#     if ask == "n":
+#         break
+# print("GOODBYE!!!")
+n=4
+for i in range(n):
+    for j in range(n - 1 - i):
+        print('for-1 i,j: ',i,j)
+    for j in range(i + 1):
+        print('for-2 i,j: ',i,j)
+    print()
