06cb855ce7979730a5cf97f1a607ecdee09d1b1a
Nguyễn Hồng Tới <nguyenhongtoi1@gmail.com>
2021-07-03 15:19:38 +0700

    Done Chapter 10

---
+++ FinalProject.py
@@ -0,0 +1,23 @@
+"""
+EXERCISE 1:     Viết phần mềm Quản Lý Sản Phẩm
+                - Mỗi danh mục có: Mã, tên; Một danh mục có nhiều sp
+                - Mỗi sản phẩm có: Mã, tên, đơn giá; Mỗi sp thuộc về 1 danh mục
+                - Cho phép: Lưu mới, sửa, xóa, tìm kiếm, sắp xếp, lưu và đọc Text File
+"""
+# Function
+# Body
+"""
+EXERCISE 2:     Viết phần mềm Quản Lý Sinh Viên
+                - Mỗi lớp có: Mã lớp, tên; một lớp có nhiều SV
+                - Mỗi SV có: mã, tên, năm sinh; Mỗi SV thuộc về 1 lớp
+                - Cho phép: Lưu mới, sửa, xóa, tìm kiếm, sắp xếp, lưu và đọc Text File
+"""
+# Function
+# Body
+"""
+EXERCISE 3: Viết phần mềm Quản Lý Sách:
+            - gồm mã sách, tên sách, năm xuất bản
+            - Cho phép: Thêm, sửa, xóa, tìm kiếm, sắp xếp, lưu và đọc Text File
+"""
+# Function
+# Body
